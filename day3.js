7268732a14eac365339c486af5b13a39c6248545
Aswin Krishna <your_email@example.com>
2026-06-23 02:03:38 +0530

    started day3

---
+++ day3.js
@@ -0,0 +1,33 @@
+// Count characters in a string
+
+const word = "aswin";
+
+// count = word.length
+
+// console.log(count)
+
+// Reverse a string
+
+// let reversed = "";
+// for (let i = word.length - 1; i >= 0; i--) {
+//   reversed = reversed + word[i];
+// }
+// console.log(reversed);
+
+// Count vowels
+
+// let vowels = 0;
+
+// for (let i = 0; i < word.length; i++) {
+//   if (
+//     word[i] === "a" ||
+//     word[i] === "e" ||
+//     word[i] === "i" ||
+//     word[i] === "o" ||
+//     word[i] === "u"
+//   ) {
+//     vowels++;
+//   }
+// }
+
+// console.log(vowels);
