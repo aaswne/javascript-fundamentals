2f3b279c39cbff63e42fb0eca4c1fa2551992161
Aswin Krishna <your_email@example.com>
2026-06-22 21:49:39 +0530

    initial commit

---
+++ day1.js
@@ -0,0 +1,35 @@
+// Print numbers 1 to 10
+
+for(let i=1; i<=10;i++){
+    console.log(i)
+}
+
+// Print even numbers from 1 to 20
+
+for(let i = 1;i<=20;i++){
+    if(i%2==0){
+        console.log(i)
+    }
+}
+
+// Print odd numbers from 1 to 20
+
+for(let i=1;i<=20;i++){
+    if(i%2!==0){
+        console.log(i)
+    }
+}
+
+// Print multiplication table of 7
+
+for(let i =1;i<=10;i++){
+    console.log(`7 * ${i} =`,7*i)
+}
+
+// Print numbers divisible by 5 from 1 to 50
+
+for(let i =1 ;i<=50;i++){
+    if(i%5==0){
+        console.log(i)
+    }
+}
