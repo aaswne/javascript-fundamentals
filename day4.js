b8386bc45c25a66198e29abec477eb4cd35ae28e
Aswin Krishna <your_email@example.com>
2026-06-25 22:02:40 +0530

    completed day1 count vowels

--- day4.js
+++ day4.js
@@ -1,50 +1,71 @@
-// Q1: Count Characters (Ignore Spaces)
+// // Q1: Count Characters (Ignore Spaces)
 
-const text = "aswin krishna";
+// const text = "aswin krishna";
 
-console.log(text.replace(" ","").length)
+// console.log(text.replace(" ","").length)
 
-// Q2: Reverse a String
+// // Q2: Reverse a String
 
-const word = "javascript";
+// const word = "javascript";
 
-let reversed = "";
+// let reversed = "";
 
-for (let i = word.length - 1; i >=0; i--) {
-  reversed = reversed + word[i];
-}console.log(reversed)
+// for (let i = word.length - 1; i >=0; i--) {
+//   reversed = reversed + word[i];
+// }console.log(reversed)
 
-// Q3: Count Vowels (Uppercase Included)
+// // Q3: Count Vowels (Uppercase Included)
 
-const word = "AsWin";
+// const word = "AsWin";
 
-let vowel = 0;
+// let vowel = 0;
 
-for (let i = 0; i < word.length; i++) {
-  if (
-    word[i].toLocaleLowerCase() === "a" ||
-    word[i].toLocaleLowerCase() === "e" ||
-    word[i].toLocaleLowerCase() === "i" ||
-    word[i].toLocaleLowerCase() === "o" ||
-    word[i].toLocaleLowerCase() === "u"
-  ) {
-    vowel++;
-  }
-}
-console.log(vowel);
+// for (let i = 0; i < word.length; i++) {
+//   if (
+//     word[i].toLocaleLowerCase() === "a" ||
+//     word[i].toLocaleLowerCase() === "e" ||
+//     word[i].toLocaleLowerCase() === "i" ||
+//     word[i].toLocaleLowerCase() === "o" ||
+//     word[i].toLocaleLowerCase() === "u"
+//   ) {
+//     vowel++;
+//   }
+// }
+// console.log(vowel);
 
 // Q4: Count Consonants (Uppercase Included)
 
-const word = "FrontEnd";
+// const word = "FrontEnd";
+
+// let constants = 0 
+
+// for(let i = 0;i<word.length;i++){
+//     if(word[i].toLowerCase()!=="a"&&
+//     word[i].toLowerCase()!=="e"&&
+//     word[i].toLowerCase()!=="i"&&
+//     word[i].toLowerCase()!=="o"&&
+//     word[i].toLowerCase()!=="u"
+// ){
+//     constants++
+// }
+// }console.log(constants)
 
 // Q5: Check Palindrome (Ignore Case)
 
+
 const word = "Madam";
 
-// Q6: Check Sentence Palindrome (Ignore Spaces)
+let palindrome = ""
 
-const text = "nurses run";
+for(let i =word.length-1;i<0;i--){
+    palindrome=palindrome+word[i]
+    
+}console.log(palindrome)
 
-// Q7: Find Most Frequent Character
+// // Q6: Check Sentence Palindrome (Ignore Spaces)
 
-const word = "javascript";
+// const text = "nurses run";
+
+// // Q7: Find Most Frequent Character
+
+// const word = "javascript";
