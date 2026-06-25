a3aea1f05bd2edc46b29f2d0e0a5181aa15442ca
Aswin Krishna <your_email@example.com>
2026-06-25 23:37:36 +0530

    completed day5 of array

--- day5.js
+++ day5.js
@@ -1,41 +1,67 @@
 // - [ ] Find the largest number in an array
 
-// const numbers = [12, 5, 8, 20, 15];
+const numbers = [12, 5, 8, 20, 15];
 
-// let largest = 0 
+let largest = 0 
 
-// for(let i = 0;i<numbers.length;i++){
-//     if(numbers[i]>largest){
-//         largest=numbers[i]
-//     }
+for(let i = 0;i<numbers.length;i++){
+    if(numbers[i]>largest){
+        largest=numbers[i]
+    }
         
-// }console.log(largest)
+}console.log(largest)
 
 
 // - [ ] Find the smallest number in an array
 
-// const numbers = [12, 5, 8, 20, 15];
+const numbers = [12, 5, 8, 20, 15];
 
-// let smallest = numbers[0]
+let smallest = numbers[0]
 
-// for (let i = 0;i<numbers.length;i++){
-//     if(numbers[i]<smallest){
-//         smallest=numbers[i]
-//     }
-// }console.log(smallest)
+for (let i = 0;i<numbers.length;i++){
+    if(numbers[i]<smallest){
+        smallest=numbers[i]
+    }
+}console.log(smallest)
 
 
 // - [ ] Calculate the sum of all array elements
-// ```js
-// const numbers = [4, 8, 12, 16, 20];
-// ```
+
+const numbers = [4, 8, 12, 16, 20];
+
+let sum = 0 
+
+
+for (let i = 0 ; i<numbers.length;i++){
+    sum=sum+numbers[i]
+}console.log(sum)
 
 // - [ ] Calculate the average of all array elements
-// ```js
-// const numbers = [10, 20, 30, 40, 50];
-// ```
+
+const numbers = [10, 20, 30, 40, 50];
+
+let sum = 0
+
+
+    let average = 0
+
+for(let i = 0 ; i<numbers.length;i++){
+    sum = sum+numbers[1]
+
+     average = sum/numbers.length
+     
+}console.log(average)
 
 // - [ ] Count how many even numbers are in the array
-// ```js
-// const numbers = [3, 6, 8, 11, 14, 17, 20];
-// ```
+
+const numbers = [3, 6, 8, 11, 14, 17, 20];
+
+let evennum = 0
+
+for (let i = 0;i<numbers.length;i++){
+    if(numbers[i]%2==0){
+        evennum++
+    }
+}console.log(evennum)
+
+
