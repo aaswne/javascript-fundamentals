6c7553fbf5ee088bccca62c90142c13d2821553b
Aswin Krishna <your_email@example.com>
2026-06-26 22:54:43 +0530

    loop nested loop

--- day6.js
+++ day6.js
@@ -8,7 +8,6 @@
 // ****
 // *****
 
-
 // for(let i =1;i<=5;i++){
 
 //     let row = ""
@@ -17,7 +16,6 @@
 //         row=row+"*"
 //     }console.log(row)
 // }
-
 
 // - [ ] Print:
 
@@ -35,7 +33,6 @@
 //     console.log(raw)
 // }
 
-
 // - [ ] Print:
 
 // 1
@@ -44,32 +41,65 @@
 // 1234
 // 12345
 
-for(let i = 1;i<=5;i++){
-    let row = ""
-    for(let j =1;j<=i;j++){
+// for(let i = 1;i<=5;i++){
+//     let row = ""
+//     for(let j =1;j<=i;j++){
 
-        row = row+j
+//         row = row+j
 
-    }console.log(row)
-}
+//     }console.log(row)
+// }
 
+// // - [ ] Print:
+// //     *
+// //    **
+// //   ***
+// //  ****
+// // *****
+
+//  for (let i = 1;i<=5;i++){
+//     let raw = ""
+//     for(let j = 5;j>i;j--)
+//         raw=raw+" "
+//     for(let s = 1;s<=i;s++){
+//         raw=raw+"*"
+//     }
+//     console.log(raw)
+//  }
 
 // - [ ] Print:
-//     *
-//    **
-//   ***
-//  ****
-// *****
 
-
-
-
-
-// - [ ] Print:
-// ```
 //     1
 //    12
 //   123
 //  1234
 // 12345
-// ```
+
+// for (let i = 1; i <= 5; i++) {
+//   let row = "";
+//   for (j = 5; j > i; j--) {
+//     row = row + " ";
+//   }
+//   for (let n = 1; n <= i; n++) {
+//     row = row + n;
+//   }
+//   console.log(row);
+// }
+
+// - [ ] Print:
+
+// 12345
+// 1234
+// 123
+// 12
+// 1
+
+for (let i = 1; i <= 5; i++) {
+  let row = "";
+
+  for (let j = 1; j <= 6 - i; j++) {
+    row = row + j;
+  }
+
+  console.log(row);
+}
