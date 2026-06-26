b0bd2167cabbd41e26d4282966c65dbd2d980be9
Aswin Krishna <your_email@example.com>
2026-06-26 23:50:06 +0530

    biharding

--- day6.js
+++ day6.js
@@ -94,12 +94,29 @@
 // 12
 // 1
 
-for (let i = 1; i <= 5; i++) {
-  let row = "";
+// for (let i = 1; i <= 5; i++) {
+//   let row = "";
 
-  for (let j = 1; j <= 6 - i; j++) {
-    row = row + j;
-  }
+//   for (let j = 1; j <= 6 - i; j++) {
+//     row = row + j;
+//   }
 
-  console.log(row);
+//   console.log(row);
+// }
+
+// - [ ] Print:
+
+
+// *
+// **
+// ***
+// ****
+// *****
+
+
+for(let i =1;i<=5;i++){
+    let raw = ""
+    for(let j = 1;j<=i;j++){
+        raw=raw+"*"
+    }console.log(raw)
 }
