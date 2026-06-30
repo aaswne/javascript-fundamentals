77d667d87eb426d8bf407eb554b82fb847a9f89a
Aswin Krishna <your_email@example.com>
2026-06-30 11:46:05 +0530

    updated main

---
+++ day9.js
@@ -0,0 +1,75 @@
+// ===============================
+// Day 9 - JavaScript String Practice
+// ===============================
+
+// Q1: Check if a string starts with a specific word.
+// Example:
+// "JavaScript" starts with "Java" -> true
+
+let word  = "javascript"
+
+console.log(word.startsWith("java"))
+
+
+// Q2: Check if a string ends with a specific word.
+// Example:
+// "photo.png" ends with ".png" -> true
+
+let str = "photo.png"
+
+console.log(str.endsWith("png"))
+
+
+// Q3: Find the index of the first occurrence of a character.
+// Example:
+// "banana"
+// Find: "a"
+// Output: 1
+
+
+
+
+// Q4: Find the index of the last occurrence of a character.
+// Example:
+// "banana"
+// Find: "a"
+// Output: 5
+
+
+// Q5: Get a part of a string using slice().
+// Example:
+// "JavaScript"
+// Output: "Script"
+
+
+// Q6: Get the first 4 characters using substring().
+// Example:
+// "Frontend"
+// Output: "Fron"
+
+
+// Q7: Get the character at a specific index.
+// Example:
+// "React"
+// Get character at index 2
+// Output: "a"
+
+
+// Q8: Repeat a string multiple times.
+// Example:
+// "Hi"
+// Repeat 3 times
+// Output: "HiHiHi"
+
+
+// Q9: Count how many times a character appears.
+// Example:
+// "banana"
+// Count how many times "a" appears.
+// Output: 3
+
+
+// Q10: Capitalize the first letter of a string.
+// Example:
+// "javascript"
+// Output: "Javascript"
